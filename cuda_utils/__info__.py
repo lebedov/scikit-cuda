4bc51deb32a0ffd8da9e9aa4c6e8871f149c8d7c
Lev Givon <lev@columbia.edu>
2010-08-09 18:46:16 -0400

    Install CUDA header files in Python header installation path. Use header installation path to find files when running kernels.

---
+++ cuda_utils/__info__.py
@@ -0,0 +1,6 @@
+# WARNING: This file is overwritten upon package installation! The
+# contents below are solely for development purposes:
+
+# Installation location of C headers:
+import os
+install_headers = os.path.realpath('.')
--- setup.py
+++ setup.py
@@ -2,14 +2,12 @@
 
 import sys
 import os
-
+from glob import glob
+from distutils.command.build_py import build_py
 from distutils.core import setup
-from distutils.extension import Extension
-from distutils.sysconfig import get_python_lib
 
 NAME =               'cuda_utils'
 VERSION =            '0.01'
-IS_RELEASED =        True
 AUTHOR =             'Lev Givon'
 AUTHOR_EMAIL =       'lev@columbia.edu'
 URL =                'http://bionet.ee.columbia.edu/code/'
@@ -27,7 +25,24 @@ CLASSIFIERS = [
     'Programming Language :: Python',
     'Topic :: Scientific/Engineering',
     'Topic :: Software Development']
+NAMESPACE_PACKAGE = 'cuda_utils'
+PACKAGES =           [NAMESPACE_PACKAGE]
 
+# Overwrite the copy of cuda_utils/__info__.py that will be installed
+# with the actual header installation path. This is necessary so that
+# PyCUDA can find the headers when executing the kernels in this
+# package that use it:
+class custom_build_py(build_py):
+    def run(self):
+        build_py.run(self)
+        package_dir = self.get_package_dir(NAMESPACE_PACKAGE)
+        inst_obj = self.distribution.command_obj['install']
+        filename = os.path.join(self.build_lib, package_dir, '__info__.py')
+        f = open(filename, 'w')
+        f.write('# Installation location of C headers:\n')
+        f.write('install_headers = \"%s\"\n' % inst_obj.install_headers)
+        f.close()
+        
 if __name__ == "__main__":
     if os.path.exists('MANIFEST'):
         os.remove('MANIFEST')
@@ -42,4 +57,7 @@ if __name__ == "__main__":
           description = DESCRIPTION,
           license = LICENSE,
           classifiers = CLASSIFIERS,
-          packages = ['cuda_utils'])
+          packages = PACKAGES,
+          headers = glob('cuda_utils/*.h'),
+          cmdclass={"build_py": custom_build_py})
+
