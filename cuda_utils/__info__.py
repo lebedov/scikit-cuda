c7110d265c040e4c7d731e3fe1ee383c4b8b619f
Lev Givon <lev@columbia.edu>
2010-08-10 17:58:12 -0400

    Set install_headers more intelligently.

--- cuda_utils/__info__.py
+++ cuda_utils/__info__.py
@@ -1,6 +1,7 @@
 # WARNING: This file is overwritten upon package installation! The
-# contents below are solely for development purposes:
+# contents below are solely for development purposes to allow one to
+# run doctests and demos from within the package directory.
 
 # Installation location of C headers:
 import os
-install_headers = os.path.realpath('.')
+install_headers = __file__.replace(os.path.basename(__file__), '')
--- cuda_utils/__init__.py
+++ cuda_utils/__init__.py
@@ -1,3 +1,6 @@
 from pkgutil import extend_path
 __path__ = extend_path(__path__, __name__)
 
+# Make the path of the CUDA header installation directory available:
+from __info__ import install_headers
+
