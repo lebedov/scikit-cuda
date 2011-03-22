841886d841cb840ee88bf4012f373a5f312e4211
Lev Givon <lev@columbia.edu>
2011-03-22 09:31:50 -0400

    Perform relative import of install_headers.

--- scikits/cuda/special.py
+++ scikits/cuda/special.py
@@ -13,7 +13,7 @@ import numpy as np
 from misc import get_dev_attrs, select_block_grid_sizes, init, get_current_device
 
 # Get installation location of C headers:
-from __init__ import install_headers
+from . import install_headers
 
 # Adapted from Cephes library:
 sici_mod_template = Template("""
