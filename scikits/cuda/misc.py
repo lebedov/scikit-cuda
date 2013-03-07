6e5c71c891b6fa6512eedd2a882dd805e2c0f6e1
Lev Givon <lev@columbia.edu>
2013-03-07 16:29:19 -0500

    Add get_soname() to misc module.

--- scikits/cuda/misc.py
+++ scikits/cuda/misc.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-General PyCUDA utility functions.
+Miscellaneous utility functions.
 """
 
 import string
@@ -769,6 +769,103 @@ def set_realloc(x_gpu, data):
     # Update the GPU memory:
     x_gpu.set(data)
 
+try:
+    import elftools
+except ImportError:
+    import re
+    import subprocess
+
+    def get_soname(filename):
+        """
+        Retrieve SONAME of shared library.
+
+        Parameters
+        ----------
+        filename : str
+            Full path to shared library.
+
+        Returns
+        -------
+        soname : str
+            SONAME of shared library.
+
+        Notes
+        -----
+        This function uses the `objdump` system command.
+        
+        """
+        
+        try:
+            out = subprocess.check_output(['objdump', '-p', filename])
+        except:
+            raise RuntimeError('error executing objdump')
+        else:
+            result = re.search('^\s+SONAME\s+(.+)$',out,re.MULTILINE)
+            if result:
+                return result.group(1)
+            else:
+
+                # No SONAME found:
+                return ''
+    
+else:
+    import ctypes
+    import elftools.elf.elffile as elffile
+    import elftools.construct.macros as macros
+    import elftools.elf.structs as structs
+
+    def get_soname(filename):
+        """
+        Retrieve SONAME of shared library.
+
+        Parameters
+        ----------
+        filename : str
+            Full path to shared library.
+
+        Returns
+        -------
+        soname : str
+            SONAME of shared library.
+
+        Notes
+        -----
+        This function uses the pyelftools [ELF] package.
+
+        References
+        ----------
+        .. [ELF] http://pypi.python.org/pypi/pyelftools
+        
+        """
+
+        stream = open(filename, 'rb')
+        f = elffile.ELFFile(stream)
+        dynamic = f.get_section_by_name('.dynamic')
+        dynstr = f.get_section_by_name('.dynstr')
+
+        # Handle libraries built for different machine architectures:         
+        if f.header['e_machine'] == 'EM_X86_64':
+            st = structs.Struct('Elf64_Dyn',
+                                macros.ULInt64('d_tag'),
+                                macros.ULInt64('d_val'))
+        elif f.header['e_machine'] == 'EM_386':
+            st = structs.Struct('Elf32_Dyn',
+                                macros.ULInt32('d_tag'),
+                                macros.ULInt32('d_val'))
+        else:
+            raise RuntimeError('unsupported machine architecture')
+
+        entsize = dynamic['sh_entsize']
+        for k in xrange(dynamic['sh_size']/entsize):
+            result = st.parse(dynamic.data()[k*entsize:(k+1)*entsize])
+
+            # The following value for the SONAME tag is specified in elf.h:  
+            if result.d_tag == 14:
+                return dynstr.get_string(result.d_val)
+
+        # No SONAME found:
+        return ''
+    
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
