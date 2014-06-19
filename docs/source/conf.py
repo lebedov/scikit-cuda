7afaa35d6a6312ef31d606e028ea32e046018a8f
Lev Givon <lev@columbia.edu>
2014-06-18 21:17:30 -0400

    Mock pycuda modules in conf.py to prevent doc build problems on RTFD.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,6 +13,33 @@
 
 import re, sys, os
 
+# Prevent project dependencies from interfering with autodoc:
+class Mock(object):
+    __all__ = []
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        else:
+            return Mock()
+
+MOCK_MODULES = ['pycuda', 'pycuda.compiler', 'pycuda.driver', 
+                'pycuda.elementwise', 'pycuda.gpuarray',
+                'pycuda.reduction', 'pycuda.scan', 'pycuda.tools', 'pytools']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
