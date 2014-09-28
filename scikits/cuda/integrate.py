7b7c6d338b3ac38303aa4df4fb078f0c253929d6
Lev Givon <lev@columbia.edu>
2014-09-28 13:02:49 -0400

    Simplify integrate.gen_trapz_mult().

--- scikits/cuda/integrate.py
+++ scikits/cuda/integrate.py
@@ -6,47 +6,17 @@ PyCUDA-based integration functions.
 
 from string import Template
 from pycuda.compiler import SourceModule
+import pycuda.elementwise as elementwise
 import pycuda.gpuarray as gpuarray
+import pycuda.tools as tools
 import numpy as np
 
-import ctypes
 import cublas
 import misc
 
 from misc import init
 
-gen_trapz_mult_template = Template("""
-#include <pycuda-complex.hpp>
-
-#if ${use_double}
-#if ${use_complex}
-#define TYPE pycuda::complex<double>
-#else
-#define TYPE double
-#endif
-#else
-#if ${use_complex}
-#define TYPE pycuda::complex<float>
-#else
-#define TYPE float
-#endif
-#endif
-
-__global__ void gen_trapz_mult(TYPE *mult, unsigned int N) {
-    unsigned int idx = blockIdx.y*blockDim.x*gridDim.x+
-                       blockIdx.x*blockDim.x+threadIdx.x;
-
-    if (idx < N) {
-        if ((idx == 0) || (idx == N-1)) {                      
-            mult[idx] = TYPE(0.5);
-        } else {
-            mult[idx] = TYPE(1.0);
-        }
-    }
-}
-""")
-
-def gen_trapz_mult(N, mult_type):
+def gen_trapz_mult(N, dtype):
     """
     Generate multiplication array for 1D trapezoidal integration.
 
@@ -58,44 +28,25 @@ def gen_trapz_mult(N, mult_type):
     ----------
     N : int
         Length of array.
-    mult_type : float type
+    dtype : float type
         Floating point type to use when generating the array.
 
     Returns
     -------
     result : pycuda.gpuarray.GPUArray
         Generated array.
-
     """
-    
-    if mult_type not in [np.float32, np.float64, np.complex64,
-                         np.complex128]:
+
+    if dtype not in [np.float32, np.float64, np.complex64,
+                     np.complex128]:
         raise ValueError('unrecognized type')
-    
-    use_double = int(mult_type in [np.float64, np.complex128])
-    use_complex = int(mult_type in [np.complex64, np.complex128])
 
-    # Allocate output matrix:
-    mult_gpu = gpuarray.empty(N, mult_type)
-
-    # Get block/grid sizes:
-    dev = misc.get_current_device()
-    block_dim, grid_dim = misc.select_block_grid_sizes(dev, N)
-
-    # Set this to False when debugging to make sure the compiled kernel is
-    # not cached:
-    cache_dir=None
-    gen_trapz_mult_mod = \
-                       SourceModule(gen_trapz_mult_template.substitute(use_double=use_double,
-                                                                       use_complex=use_complex),
-                                    cache_dir=cache_dir)
-
-    gen_trapz_mult = gen_trapz_mult_mod.get_function("gen_trapz_mult")    
-    gen_trapz_mult(mult_gpu, np.uint32(N),
-                   block=block_dim,
-                   grid=grid_dim)
-    
-    return mult_gpu
+    ctype = tools.dtype_to_ctype(dtype)
+    func = elementwise.ElementwiseKernel("{ctype} *x".format(ctype=ctype),
+                                         "x[i] = ((i == 0) || (i == {M})) ? 0.5 : 1".format(M=N-1))
+    x_gpu = gpuarray.empty(N, dtype)
+    func(x_gpu)
+    return x_gpu
 
 def trapz(x_gpu, dx=1.0, handle=None):
     """
@@ -127,8 +78,7 @@ def trapz(x_gpu, dx=1.0, handle=None):
     >>> x_gpu = gpuarray.to_gpu(x)
     >>> z = integrate.trapz(x_gpu)
     >>> np.allclose(np.trapz(x), z)
-    True
-    
+    True    
     """
 
     if handle is None:
@@ -193,7 +143,7 @@ __global__ void gen_trapz2d_mult(TYPE *mult,
 }
 """)
 
-def gen_trapz2d_mult(mat_shape, mult_type):
+def gen_trapz2d_mult(mat_shape, dtype):
     """
     Generate multiplication matrix for 2D trapezoidal integration.
 
@@ -205,26 +155,25 @@ def gen_trapz2d_mult(mat_shape, mult_type):
     ----------
     mat_shape : tuple
         Shape of matrix.
-    mult_type : float type
+    dtype : float type
         Floating point type to use when generating the array.
 
     Returns
     -------
     result : pycuda.gpuarray.GPUArray
         Generated matrix.
-
     """
 
-    if mult_type not in [np.float32, np.float64, np.complex64,
+    if dtype not in [np.float32, np.float64, np.complex64,
                          np.complex128]:
         raise ValueError('unrecognized type')
     
-    use_double = int(mult_type in [np.float64, np.complex128])
-    use_complex = int(mult_type in [np.complex64, np.complex128])
+    use_double = int(dtype in [np.float64, np.complex128])
+    use_complex = int(dtype in [np.complex64, np.complex128])
 
     # Allocate output matrix:
     Ny, Nx = mat_shape
-    mult_gpu = gpuarray.empty(mat_shape, mult_type)
+    mult_gpu = gpuarray.empty(mat_shape, dtype)
 
     # Get block/grid sizes:
     dev = misc.get_current_device()
@@ -278,7 +227,6 @@ def trapz2d(x_gpu, dx=1.0, dy=1.0, handle=None):
     >>> z = integrate.trapz2d(x_gpu)
     >>> np.allclose(np.trapz(np.trapz(x)), z)
     True
-
     """
 
     if handle is None:
