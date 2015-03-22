0e3b86bd34ac7eee7aad6d1bbbd02122b654906b
Lev Givon <lev@columbia.edu>
2015-03-22 08:38:37 -0400

    Bump release.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 NAME =               'scikits.cuda'
-VERSION =            '0.5.0a3'
+VERSION =            '0.5.0b1'
 AUTHOR =             'Lev Givon'
 AUTHOR_EMAIL =       'lev@columbia.edu'
 URL =                'https://github.com/lebedov/scikits.cuda/'
