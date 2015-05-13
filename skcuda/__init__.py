dfe4cef48f53a7ce46071384b1306775aeda655f
Lev Givon <lev@columbia.edu>
2015-05-13 12:57:06 -0400

    Rearrange order of imports in skcuda/__init__.py so that import skcuda.blah works with Python 3.

--- skcuda/__init__.py
+++ skcuda/__init__.py
@@ -1,9 +1,13 @@
 import os
+
+# This import must precede the invocation of extend_path() to work with Python
+# 3:
+import scikits.cuda
+
 from pkgutil import extend_path
 __path__ = extend_path(__path__, 'scikits.cuda')
 
 # Needed to ensure correct header location even when modules are imported as
 # skcuda.something:
-import scikits.cuda
 install_headers = \
     scikits.cuda.__file__.replace(os.path.basename(scikits.cuda.__file__), '') + 'include'
