fd3c14b71876e3d1afdd2915110cde30cb3430ba
Lev Givon <lev@columbia.edu>
2015-06-22 09:14:19 -0400

    Add deprecation warning for scikits.cuda namespace package (skcuda will become the main namespace).

--- scikits/cuda/__init__.py
+++ scikits/cuda/__init__.py
@@ -1,5 +1,10 @@
 from __future__ import absolute_import
 
+import warnings
+warnings.warn('The scikits.cuda namespace package is deprecated and will be '
+              'removed in the future; please import the skcuda package '
+              'instead.', DeprecationWarning, stacklevel=2)
+
 from pkgutil import extend_path
 __path__ = extend_path(__path__, __name__)
 
