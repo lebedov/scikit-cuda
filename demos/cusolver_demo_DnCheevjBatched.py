cb7cc1b2376feb3534ef6b89c89aa845229153dd
Yiyin Zhou <yiyin@ee.columbia.edu>
2017-10-24 18:17:52 -0400

    updated demos

--- demos/cusolver_demo_DnCheevjBatched.py
+++ demos/cusolver_demo_DnCheevjBatched.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 """
-Demo of how to call low-level CUSOLVER wrappers to perform SVD decomposition.
+Demo of how to call low-level CUSOLVER wrappers to perform eigen decomposition
+for a batch of small Hermitian matrices.
 """
 
 import numpy as np
@@ -21,48 +22,49 @@ for i in range(batchSize):
     x = x+x.conj().T
     x = x.astype(np.complex64)
     A[i*n:(i+1)*n, :] = x
+    # Need to reverse dimensions because CUSOLVER expects column-major matrices:
     B[i*n:(i+1)*n, :] = x.T.copy()
 
-# Need to reverse dimensions because CUSOLVER expects column-major matrices:
 x_gpu = gpuarray.to_gpu(B)
 
 # Set up output buffers:
 w_gpu = gpuarray.empty((batchSize, n), dtype = np.float32)
 
-# Set up work buffers:
+# Set up parameters
 params = solver.cusolverDnCreateSyevjInfo()
 solver.cusolverDnXsyevjSetTolerance(params, 1e-7)
 solver.cusolverDnXsyevjSetMaxSweeps(params, 15)
 
-
+# Set up work buffers:
 lwork = solver.cusolverDnCheevjBatched_bufferSize(handle, 'CUSOLVER_EIG_MODE_VECTOR',
                                     'u', n, x_gpu.gpudata, n,
                                     w_gpu.gpudata, params, batchSize)
-print lwork
+
 workspace_gpu = gpuarray.zeros(lwork, dtype = A.dtype)
 info = gpuarray.zeros(batchSize, dtype = np.int32)
+
 # Compute:
 solver.cusolverDnCheevjBatched(handle, 'CUSOLVER_EIG_MODE_VECTOR',
                        'u', n, x_gpu.gpudata, n,
                         w_gpu.gpudata, workspace_gpu.gpudata,
                         lwork, info.gpudata, params, batchSize)
 
-# print solver.cusolverDnXsyevjGetSweeps(handle, params)
-# print solver.cusolverDnXsyevjGetResidual(handle, params)
-
+# Print info
 tmp = info.get()
 if any(tmp):
     print "the following job did not converge:", np.nonzero(tmp)[0]
 else:
     print "all jobs converged"
-# print info
+
+# Destroy handle
 solver.cusolverDnDestroySyevjInfo(params)
 solver.cusolverDnDestroy(handle)
 
 Q = x_gpu.get()
 W = w_gpu.get()
+print 'maximum error in A * Q - Q * Lambda is:'
 for i in range(batchSize):
     q = Q[i*n:(i+1)*n,:].T.copy()
     x = A[i*n:(i+1)*n,:].copy()
     w = W[i, :].copy()
-    print i, np.abs(np.dot(x, q) - np.dot(q, np.diag(w))).max()
+    print '{}th matrix'.format(i), np.abs(np.dot(x, q) - np.dot(q, np.diag(w))).max()
--- demos/cusolver_demo_DnZheevj.py
+++ demos/cusolver_demo_DnZheevj.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 """
-Demo of how to call low-level CUSOLVER wrappers to perform SVD decomposition.
+Demo of how to call low-level CUSOLVER wrappers to perform eigen decomposition
+for Hermitian matrices.
 """
 
 import numpy as np
@@ -20,30 +21,33 @@ x_gpu = gpuarray.to_gpu(x.T.copy())
 # Set up output buffers:
 w = gpuarray.empty(n, dtype = np.double)
 
-# Set up work buffers:
+# Set up parameters
 params = solver.cusolverDnCreateSyevjInfo()
 solver.cusolverDnXsyevjSetTolerance(params, 1e-7)
 solver.cusolverDnXsyevjSetMaxSweeps(params, 15)
 
-
+# Set up work buffers:
 lwork = solver.cusolverDnZheevj_bufferSize(handle, 'CUSOLVER_EIG_MODE_VECTOR',
                                     'u', n, x_gpu.gpudata, m,
                                     w.gpudata, params)
-print lwork
+
 workspace_gpu = gpuarray.zeros(lwork, dtype = x.dtype)
 info = gpuarray.zeros(1, dtype = np.int32)
+
 # Compute:
 solver.cusolverDnZheevj(handle, 'CUSOLVER_EIG_MODE_VECTOR',
                        'u', n, x_gpu.gpudata, m,
                         w.gpudata, workspace_gpu.gpudata,
                         lwork, info.gpudata, params)
 
+# Print info
 print solver.cusolverDnXsyevjGetSweeps(handle, params)
 print solver.cusolverDnXsyevjGetResidual(handle, params)
 
-# print info
+# Destroy handle
 solver.cusolverDnDestroySyevjInfo(params)
 solver.cusolverDnDestroy(handle)
 
+# Check error
 Q = x_gpu.get().T
-print np.abs(np.dot(x, Q) - np.dot(Q, np.diag(w.get()))).max()
+print 'maximum error in A * Q - Q * Lambda is:', np.abs(np.dot(x, Q) - np.dot(Q, np.diag(w.get()))).max()
