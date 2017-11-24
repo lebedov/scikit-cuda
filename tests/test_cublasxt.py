a8ac092615a935c0bebf5c83be64f3edc3c213ea
Lev E. Givon <lev@columbia.edu>
2017-11-24 15:17:46 -0500

    Fix CUBLASXT bindings and tests (#181).

--- tests/test_cublasxt.py
+++ tests/test_cublasxt.py
@@ -8,15 +8,18 @@ from unittest import main, makeSuite, TestCase, TestSuite
 
 import numpy as np
 
+import pycuda.autoinit
 import skcuda.cublasxt as cublasxt
+import skcuda.misc as misc
 
 class test_cublasxt(TestCase):
     def setUp(self):
+        np.random.seed(0)
         self.handle = cublasxt.cublasXtCreate()
         self.nbDevices = 1
         self.deviceId = np.array([0], np.int32)
         cublasxt.cublasXtDeviceSelect(self.handle, self.nbDevices,
-                                      self.deviceId.ctypes.data)
+                                      self.deviceId)
 
     def tearDown(self):
         cublasxt.cublasXtDestroy(self.handle)
@@ -25,9 +28,9 @@ class test_cublasxt(TestCase):
         a = np.random.rand(4, 4).astype(np.float32)
         b = np.random.rand(4, 4).astype(np.float32)
         c = np.zeros((4, 4), np.float32)
-
-        cublasxt.cublasXtSgemm(self.handle, cublasxt._CUBLAS_OP['N'],
-                               cublasxt._CUBLAS_OP['N'], 4, 4, 4, np.float32(1.0),
+        cublasxt.cublasXtSgemm(self.handle,
+                               'N', 'N', 
+                               4, 4, 4, np.float32(1.0),
                                a.ctypes.data, 4, b.ctypes.data, 4, np.float32(0.0),
                                c.ctypes.data, 4)
         np.allclose(np.dot(b.T, a.T).T, c)
@@ -37,9 +40,10 @@ class test_cublasxt(TestCase):
         b = np.random.rand(4, 4).astype(np.float64)
         c = np.zeros((4, 4), np.float64)
         
-        cublasxt.cublasXtDgemm(self.handle, cublasxt._CUBLAS_OP['N'],
-                               cublasxt._CUBLAS_OP['N'], 4, 4, 4, np.float64(1.0),
-                               a.ctypes.data, 4, b.ctypes.data, 4, np.float64(0.0),
+        cublasxt.cublasXtDgemm(self.handle,
+                               'N', 'N', 4, 4, 4,
+                               np.float64(1.0),
+                               a.ctypes.data, 4, b.ctypes.data, 4, np.float64(0.0),                               
                                c.ctypes.data, 4)
         np.allclose(np.dot(b.T, a.T).T, c)
 
@@ -48,8 +52,9 @@ class test_cublasxt(TestCase):
         b = (np.random.rand(4, 4)+1j*np.random.rand(4, 4)).astype(np.complex128)
         c = np.zeros((4, 4), np.complex128)
         
-        cublasxt.cublasXtCgemm(self.handle, cublasxt._CUBLAS_OP['N'],
-                               cublasxt._CUBLAS_OP['N'], 4, 4, 4, np.complex128(1.0),
+        cublasxt.cublasXtCgemm(self.handle,
+                               'N', 'N', 4, 4, 4,           
+                               np.complex128(1.0),
                                a.ctypes.data, 4, b.ctypes.data, 4, np.complex128(0.0),
                                c.ctypes.data, 4)
         np.allclose(np.dot(b.T, a.T).T, c)
@@ -59,11 +64,21 @@ class test_cublasxt(TestCase):
         b = (np.random.rand(4, 4)+1j*np.random.rand(4, 4)).astype(np.complex256)
         c = np.zeros((4, 4), np.complex256)
         
-        cublasxt.cublasXtZgemm(self.handle, cublasxt._CUBLAS_OP['N'],
-                               cublasxt._CUBLAS_OP['N'], 4, 4, 4, np.complex256(1.0),
+        cublasxt.cublasXtZgemm(self.handle,
+                               'N', 'N', 4, 4, 4,
+                               np.complex256(1.0),
                                a.ctypes.data, 4, b.ctypes.data, 4, np.complex256(0.0),
                                c.ctypes.data, 4)
         np.allclose(np.dot(b.T, a.T).T, c)
 
+def suite():
+    s = TestSuite()
+    s.addTest(test_cublasxt('test_cublasXtSgemm'))
+    s.addTest(test_cublasxt('test_cublasXtCgemm'))
+    if misc.get_compute_capability(pycuda.autoinit.device) >= 1.3:
+        s.addTest(test_cublasxt('test_cublasXtDgemm'))
+        s.addTest(test_cublasxt('test_cublasXtZgemm'))
+    return s
+
 if __name__ == '__main__':
-    main()
+    main(defaultTest = 'suite')
