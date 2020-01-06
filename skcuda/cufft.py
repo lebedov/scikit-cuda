353334bc70135ef479cb25f1d34a0e50d256b1d2
Lev E. Givon <lev@columbia.edu>
2020-01-06 14:49:18 -0500

    Update cublas, cufft, and cusolver wrappers to find respective libraries on CUDA 10.2.

--- skcuda/cufft.py
+++ skcuda/cufft.py
@@ -13,7 +13,7 @@ import sys
 
 # Load library:
 _linux_version_list = [10.1, 10.0, 9.2, 9.1, 9.0, 8.0, 7.5, 7.0, 6.5, 6.0, 5.5, 5.0, 4.0]
-_win32_version_list = [10, 100, 92, 91, 90, 80, 75, 70, 65, 60, 55, 50, 40]
+_win32_version_list = [10, 10, 100, 92, 91, 90, 80, 75, 70, 65, 60, 55, 50, 40]
 if 'linux' in sys.platform:
     _libcufft_libname_list = ['libcufft.so'] + \
                              ['libcufft.so.%s' % v for v in _linux_version_list]
